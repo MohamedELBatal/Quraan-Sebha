9afa75d89bac94c47b06e3143539ea335aef97db
Development <beshovetch@gmail.com>
2024-02-20 17:54:45 +0400

    localization_delegates_added

--- lib/home/tabs/settings_tab.dart
+++ lib/home/tabs/settings_tab.dart
@@ -5,8 +5,34 @@ class SettingsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 40,horizontal: 18),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Language",style:Theme.of(context).textTheme.bodyLarge,),
+          Container(
+            padding: EdgeInsets.all(12),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFFB7935F)),
+              borderRadius: BorderRadius.circular(25),),
+            child: Text("Arabic",style:Theme.of(context).textTheme.bodyMedium,),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height*.03,
+          ),
+          Text("Theme",style:Theme.of(context).textTheme.bodyLarge,),
+          Container(
+            padding: EdgeInsets.all(12),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              border: Border.all(color: Color(0xFFB7935F)),
+              borderRadius: BorderRadius.circular(25),),
+            child: Text("Light",style:Theme.of(context).textTheme.bodyMedium,),
+          ),
+        ],
+      ),
     );
   }
 }
